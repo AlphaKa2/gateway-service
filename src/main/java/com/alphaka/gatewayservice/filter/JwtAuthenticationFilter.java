ddabf85e443402bc54884d870e43a037f50c3bbd
chanhoim <cksgh201@naver.com>
2024-10-11 20:26:57 +0900

    AK-124 JWT 검증 필터 구현
    인증이 필요한 요청에 대해 JWT 검증,
    토큰이 없는 경우 -> 로그인 페이지 리다이렉션
    토큰이 만료된 경우 -> 401 응답, 프론트에서 재발급 요청 처리
    토큰이 유효한 경우 -> 사용자 정보를 헤더에 담아, 각 서비스에서 인가에 사용

---
+++ src/main/java/com/alphaka/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.alphaka.gatewayservice.filter;
+
+import com.alphaka.gatewayservice.exception.TokenExpiredException;
+import com.alphaka.gatewayservice.jwt.JwtService;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    public static final String AUTHENTICATED_USER_EMAIL_HEADER = "X-User-Email";
+    public static final String AUTHENTICATED_USER_ROLE_HEADER = "X-User-Role";
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        super(Config.class);
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            Optional<String> maybeAccessToken = jwtService.extractAccessToken(exchange.getRequest());
+
+            //accessToken이 없으면 login 페이지로 리다이렉션
+            if (maybeAccessToken.isEmpty()) {
+                return handleRedirect(exchange, config.loginPageRedirectionUrl);
+            }
+
+            String accessToken = maybeAccessToken.get();
+            if (!jwtService.isValidToken(accessToken)) { //유효하지 않다면 401 응답, 프론트는 인증 서비스에 재발급 요청
+                throw new TokenExpiredException();
+            }
+
+            //유효한 토큰이라면, 유저 정보를 요청 헤더에 추가
+            return chain.filter(setAuthenticationHeader(exchange, jwtService.extractUserInformation(accessToken)));
+        };
+    }
+
+    private ServerWebExchange setAuthenticationHeader(ServerWebExchange exchange, Map<String, String> userInformation) {
+        return exchange.mutate()
+                .request(r -> r.headers(headers -> {
+                    headers.add(AUTHENTICATED_USER_EMAIL_HEADER, userInformation.get(JwtService.EMAIL_CLAIM));
+                    headers.add(AUTHENTICATED_USER_ROLE_HEADER, userInformation.get(JwtService.ROLE_CLAIM));
+                }))
+                .build();
+    }
+
+    private Mono<Void> handleRedirect(ServerWebExchange exchange, String redirectionUrl) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.SEE_OTHER);
+        response.getHeaders().set("Location", redirectionUrl);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    static class Config {
+        private String loginPageRedirectionUrl;
+    }
+}
+
